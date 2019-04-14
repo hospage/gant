91a69dd6f98cfea856b22bfcf74dacb5b74b1917
jedel1043 <jedel0124@gmail.com>
2019-04-14 12:52:32 -0500

    improvement: Elimina pruebas de Task

--- js/tests.js
+++ js/tests.js
@@ -1,9 +1,7 @@
 /*
     31-Marzo-2019
-    Pruebas de creación de un objeto User y un objeto Task
+    Pruebas de creación de un objeto User
 */
 let userTest = new User("Juan", roleName.ADMIN);
-let taskTest = new Task("tarea1", null, new Date(2019, 2, 1), new Date(2019, 2, 8), taskType.TASK);
 console.log(userTest.getName());
 console.log(userTest.getRole());
-console.log(taskTest.getBeginDate().getMonth());
